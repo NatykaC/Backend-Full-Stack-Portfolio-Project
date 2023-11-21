a7688a7043c878417e4e5253e782fc987d7b4dfe
Natyka Callwood <Natykacallwood@pursuit.org>
2023-11-21 00:19:09 -0500

    Added more data to the seed.sql file so I could have a full page of crystals.

--- db/seed.sql
+++ db/seed.sql
@@ -1,7 +1,15 @@
 \c crystals_dev;
 
 INSERT INTO crystals (name, color, hardness, rarity, can_be_water_cleansed, healing_effects, collected_on, favorite) VALUES
-('Amethyst', 'Pale lilac to dark vivid purple', 7, 'Easily obtained', true, 'Calms nervous system and the brain, easing headaches.', '2023-11-11', true),
-('Prehnite', 'Pale green, yellow, white, colorless', 6, 'Easily obtained', true, 'Release emotional pain from the past.', '2023-05-02', false),
-('Red Jasper', 'Red, yellow, brown', 6, 'Easily obtained', true, 'Gently energizing and suportive. Grounding and protecting the body strengthening the energy field.', '2023-04-11', true),
-('Selenite', 'Clear', 2, 'Easily obtained', false, 'A conductor of energy, and its cleansing effects can be strongly felt on the system.', '2023-06-27', true);
+('Amethyst', 'Pale lilac ', 7, 'Easily obtained', true, 'Calms nervous system and the brain, easing headaches.', '2023-11-11', true),
+('Prehnite', 'Pale green', 6, 'Easily obtained', true, 'Release emotional pain from the past.', '2023-05-02', false),
+('Red Jasper', 'Red', 6, 'Easily obtained', true, 'Gently energizing and suportive. Grounding and protecting the body strengthening the energy field.', '2023-04-11', true),
+('Selenite', 'Clear', 2, 'Easily obtained', false, 'A conductor of energy, and its cleansing effects can be strongly felt on the system.', '2023-06-27', true),
+('Spinel', 'Reddish brown', 8, 'Rare', true, 'Detoxifies the body and is also capable of healing emotional toxicity', '2023-04-18', false),
+('Aragonite', 'Orange-Red', 4, 'Rare', false, 'Helps support the skeleton and the bone, the physical frame of the body. Gently increases circulation.', '2023-02-27', false),
+('Azurite', 'Deep sapphire blue', 3, 'Rare', false, 'Enhances intuition and psychic abilities, stimulates creativity, and helps clear throat problems.', '2023-05-29', true),
+('Tigers Eye', 'Golden yellow', 7, 'Very easily obtained', true, 'Supports a healthy metabolism and prevents physical energy from becoming depleted.', '2023-01-11', true),
+('Moonstone', 'Whitish Gray', 6, 'Easliy obtained', true, 'Increases creativity and enhances intuition.', '2023-09-18', true),
+('Jet', 'Black', 3, 'Easily obtained', false, 'Helps with the recall and healing of past lives persisting into this life. Helps improve memory.', '2023-12-14', true),
+('Carnelian', 'Orange', 7, 'Very easily obtained', true, 'Speeds up healing processes especially after trauma or injury. Helpin bring courage when facing personal challenges.', '2023-11-30', false),
+('Lepidolite', 'Pink', 2, 'Easily obtained', false, 'Helps with insomnia, relieves physical, mental, and emotional exhaustion. Helps uplift the mind.', '2023-03-29', true);
