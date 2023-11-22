23fed405f78279e10bcc939d623a404af1dbb465
Natyka Callwood <Natykacallwood@pursuit.org>
2023-11-21 23:39:11 -0500

    Cleaned up the validations file.

--- validations/checkCrystals.js
+++ validations/checkCrystals.js
@@ -6,16 +6,6 @@ const checkName = (req, res, next)=>{
     }
 };
 
-const checkBooleans = (req, res, next)=>{
-    const fav = req.body.favorite
-    const waterCleansed = req.body.can_be_water_cleansed
-    if ((fav == "true" || fav == "false" || fav == undefined || typeof fav == "boolean") 
-    & (waterCleansed == "true" || waterCleansed == "false" || waterCleansed == undefined || typeof waterCleansed == "boolean")){
-    } else {
-        res.status(400).json({error: "favorite and can_be_water_cleansed must be a boolean value"})
-    }
-};
-
 const checkColor = (req, res, next)=>{
     if (req.body.color){
         return next()
@@ -28,6 +18,5 @@ const checkColor = (req, res, next)=>{
 
 module.exports = {
     checkName,
-    checkBooleans, 
-    checkColor,
+    checkColor
 }
